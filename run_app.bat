d23de0307cf67870932ff657bf2a2195da3521e1
MAHMOUD AYMAN <105120867+hoka83@users.noreply.github.com>
2026-01-02 00:35:24 +0200

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Starting Sensor Simulator...
-start cmd /k "cd "D:\Real-Time Production Line Sensor Dashboard with Remote\sensor simulator" && python simulator.py"
+start cmd /k "cd "sensor simulator" && python simulator.py"
 
 timeout /t 2 >nul
 
 echo Starting Application...
-start cmd /k "cd "D:\Real-Time Production Line Sensor Dashboard with Remote\app" && python main.py"
+start cmd /k "cd "app" && python main.py"
